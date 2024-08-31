7ec9cfc655a68f74f4c9023744239ba2c1347c55
Abhishek Singh <abhishek.dumca@gmail.com>
2024-08-31 16:39:24 +0530

    color highlights and terminal keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,4 +3,5 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
-vim.keymap.set("n", "<leader>ft", "<CMD>ToggleTerm direction=float<CR>", { desc = "Toggle terminal command" })
+vim.keymap.set("n", "<c-\\>", "<CMD>ToggleTerm direction=float<CR>", { desc = "Toggle terminal command" })
+vim.keymap.set("t", "<c-\\>", "<CMD>ToggleTerm<CR>", { desc = "Toggle terminal command" })
